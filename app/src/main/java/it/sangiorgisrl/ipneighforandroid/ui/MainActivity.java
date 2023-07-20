6a67969122e2ce9ff4d7f512bc66c7aa6fb7eb57
K120599 <139533165+K120599@users.noreply.github.com>
2023-07-20 13:08:54 +0530

    Update MainActivity.java

--- app/src/main/java/it/sangiorgisrl/ipneighforandroid/ui/MainActivity.java
+++ app/src/main/java/it/sangiorgisrl/ipneighforandroid/ui/MainActivity.java
@@ -7,9 +7,12 @@ import android.widget.TextView;
 import it.alessangiorgi.ipneigh30.ArpNDK;
 import it.sangiorgisrl.ipneighforandroid.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView arptable_res;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
 
         arptable_res = findViewById(R.id.arptable);
 
-        arptable_res.setText(ArpNDK.getARP());
+        // Get the ArrayList from ArpNDK.getARP()
+        ArrayList<String> arpList = ArpNDK.getARP();
+
+        // Convert the ArrayList elements to a single string
+        StringBuilder arpString = new StringBuilder();
+        for (String arpEntry : arpList) {
+            arpString.append(arpEntry).append("\n"); // Append each entry with a new line
+        }
+
+        // Set the converted string as the text of the TextView
+        arptable_res.setText(arpString.toString());
     }
 }
