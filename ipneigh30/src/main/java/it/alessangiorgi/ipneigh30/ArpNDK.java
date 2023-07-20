c440255f6746ebaab3ae03b08f5df5936cba42d2
K120599 <139533165+K120599@users.noreply.github.com>
2023-07-20 14:37:19 +0530

    Update ArpNDK.java

--- ipneigh30/src/main/java/it/alessangiorgi/ipneigh30/ArpNDK.java
+++ ipneigh30/src/main/java/it/alessangiorgi/ipneigh30/ArpNDK.java
@@ -74,11 +74,11 @@ public class ArpNDK {
     @Override
     public String toString() {
         return "{" +
-                "ip:'" + ip + '\'' +
-                ", device:'" + device + '\'' +
-                ", wlan:'" + wlan + '\'' +
-                ", lla:'" + lla + '\'' +
-                ", mac:'" + mac + '\'' +
+                "ip='" + ip + '\'' +
+                ", device='" + device + '\'' +
+                ", wlan='" + wlan + '\'' +
+                ", lla='" + lla + '\'' +
+                ", mac='" + mac + '\'' +
                 '}';
     }
 }
