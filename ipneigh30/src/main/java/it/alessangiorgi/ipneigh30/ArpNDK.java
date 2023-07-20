a3346f8c6b51fbe7d20736513ef780a7ea36eef2
K120599 <139533165+K120599@users.noreply.github.com>
2023-07-20 12:29:01 +0530

    Update ArpNDK.java

--- ipneigh30/src/main/java/it/alessangiorgi/ipneigh30/ArpNDK.java
+++ ipneigh30/src/main/java/it/alessangiorgi/ipneigh30/ArpNDK.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-
+import java.util.ArrayList;
 
 public class ArpNDK {
 
